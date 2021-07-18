b67c28aec4745d3d7b8ebfad0072073cf955a141
Tatev84 <tatevh84@gmail.com>
2021-07-18 22:23:59 +0400

    homework 6

---
+++ script.js
@@ -0,0 +1,52 @@
+/*  function triangle (a,h){
+    return a*h/2
+ 
+}
+ let a= triangle(7,10)
+    console.log(a) 
+       */
+
+/* 
+   function arr(a){
+      for(var i=0; i<a.length; i++){
+      for (var j=0; j<a[i].length; j++){
+      if(a[i][j]%3===0){
+        console.log(a[i][j])
+          
+     }
+   }
+}
+
+}
+arr([[15,61,12],[21, 5, 33]]) */
+
+
+
+/* 
+ function f(num){
+    var fact=1;
+     for(var i=1; i<=num;i++){
+
+     fact*=i
+     console.log(fact)
+    }
+ }
+ f() */
+
+
+ 
+/*  function f(b){
+ return Math.abs(b);
+ }
+ console.log(f(-10)) */
+
+/* function f(a){
+    return Math.ceil(a);
+}
+ console.log (f(5.2)) */
+
+
+ function c(d){
+    return Math.floor(d);
+}
+console.log (c(6.2)) 
